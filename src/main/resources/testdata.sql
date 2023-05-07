c26355b728fb43c478fa24975b802dc566f1fee7
GjergjiSh <gjergji_shkurti@yahoo.com>
2023-05-07 16:24:56 +0200

    Added test data initialization for students

---
+++ src/main/resources/testdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO students (name, matnr, lastname) VALUES ('John', '12345', 'Doe');
+INSERT INTO students (name, matnr, lastname) VALUES ('Jane', '67890', 'Smith');
+INSERT INTO students (name, matnr, lastname) VALUES ('Bob', '24680', 'Johnson');
