4f88501f17d2fcc6fb5bbcfe71b66bab08ab4fb1
volko <volkovskiy4@gmail.com>
2023-05-13 14:59:19 +0200

    Fixed repo

--- src/main/java/com/dbproject/ezexam/repositories/UserRepo.java
+++ src/main/java/com/dbproject/ezexam/repositories/UserRepo.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepo extends JpaRepository<Student, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
