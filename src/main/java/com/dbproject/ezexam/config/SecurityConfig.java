f46c8de282fa9f85ba7013ce2f5ff3c2b5218b22
volko <volkovskiy4@gmail.com>
2023-05-25 15:34:47 +0200

    Fixed security config

--- src/main/java/com/dbproject/ezexam/config/SecurityConfig.java
+++ src/main/java/com/dbproject/ezexam/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin(formLogin -> formLogin
