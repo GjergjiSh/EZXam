affc289d39a2e2bf2ad1dc16b4a64b0537248444
GjergjiSh <gjergji_shkurti@yahoo.com>
2023-05-28 02:29:06 +0200

    Add class level setter to question

--- src/main/java/com/dbproject/ezexam/entities/Question.java
+++ src/main/java/com/dbproject/ezexam/entities/Question.java
@@ -6,10 +6,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @Table(name = "questions")
 @Entity
 public class Question {
